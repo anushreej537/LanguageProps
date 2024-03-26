27a594eea28463edc4b767ea81088c246190fb42
anushreej537 <anushreej537@gmail.com>
2024-03-26 15:11:58 +0530

    'work'

---
+++ src/Data.js
@@ -0,0 +1,12 @@
+// export let Data=[
+//     {name:'java',duration:'1 year',fees:280000,img:"https://tse4.mm.bing.net/th?id=OIP.tKRpLElMmGvi8z7DKJnBpwHaF1&pid=Api&P=0"},
+//     {name:'python',duration:"1 year",fees:270000,img:"https://tse4.mm.bing.net/th?id=OIP.tKRpLElMmGvi8z7DKJnBpwHaF1&pid=Api&P=0"},
+//     {name:'javascript',duration:"1 year",fees:230000,img:"https://tse4.mm.bing.net/th?id=OIP.tKRpLElMmGvi8z7DKJnBpwHaF1&pid=Api&P=0"}
+// ];
+
+let Data = [
+    { name: 'vivoT1', duration: '2 year', fees: 25000 },
+    { name: 'oppo', duration: '1 year', fees: 30000 },
+    { name: 'sumsung', duration: '4 year', fees: 40000 }
+];
+export default Data;
---
+++ src/components/Lang.jsx
@@ -0,0 +1,24 @@
+// import React from "react"
+// const Lang=({name,duration,fees,img})=>{
+//     return (
+//         <>
+//         <h1>Name : {name}</h1>
+//         <h1>duration : {duration}</h1> 
+//         <h1>fees : {fees}</h1>
+//         <h1>image : {img}</h1>
+//         </>
+//     )
+// }
+// export default Lang;
+
+import React from "react";
+const Lang=({name,duration,fees})=>{
+    return(
+        <>
+        <h1>name:{name}</h1>
+        <h1>duration:{duration}</h1>
+        <h1>fees:{fees}</h1>
+        </>
+    )
+}
+export default Lang
